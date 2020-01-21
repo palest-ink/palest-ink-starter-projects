00cc5e4b7b478a78b1a7565c26c261c40be91285
Auguest-Z <1559018677@qq.com>
2020-01-21 17:05:41 +0800

    增加代码注释，优化嵌套多层的代码

---
+++ palest-ink-spring-boot-starter-parent/kepler-zk-spring-boot-starter/src/main/java/com/palest/ink/zk/integration/listener/AbstractPathNodeDataConsumer.java
@@ -0,0 +1,93 @@
+package com.palest.ink.zk.integration.listener;
+
+import com.alibaba.fastjson.JSON;
+import com.palest.ink.zk.AfterUpdate;
+import com.palest.ink.zk.ZkEventData;
+import com.palest.ink.zk.ZkEventType;
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 通过注解的反射调用的适配器
+ *
+ * @author WeiHui
+ * @date 2019/1/21
+ */
+public abstract class AbstractPathNodeDataConsumer implements PathNodeDataConsumer {
+
+	/**
+	 * 事件对应的方法缓存
+	 */
+	private Map<ZkEventType, List<Method>> methodMap = new ConcurrentHashMap<>();
+
+	/**
+	 * 构造方法初始化-注解对应的方法，并加入缓存
+	 */
+	public AbstractPathNodeDataConsumer() {
+		initMethodCache();
+	}
+
+	/**
+	 * 方法缓存初始化
+	 */
+	private void initMethodCache() {
+		Method[] methods = this.getClass().getMethods();
+		if (ArrayUtils.isEmpty(methods)) {
+			return;
+		}
+		for (Method method : methods) {
+			AfterUpdate afterUpdate = method.getAnnotation(AfterUpdate.class);
+			if (afterUpdate != null) {
+				ZkEventType[] eventTypes = afterUpdate.value();
+				if (ArrayUtils.isNotEmpty(eventTypes)) {
+					Arrays.stream(eventTypes).forEach(k -> {
+						methodMap.computeIfAbsent(k, v -> new LinkedList<>());
+						methodMap.get(k).add(method);
+					});
+				}
+			}
+		}
+	}
+
+	/**
+	 * 消费
+	 *
+	 * @param eventData 事件数据
+	 */
+	@Override
+	public final void consume(ZkEventData eventData) {
+		List<Method> methods = methodMap.get(eventData.getEventType());
+		if (!CollectionUtils.isEmpty(methods)) {
+			methods.forEach(m -> ReflectionUtils.invokeMethod(m, this, extractMessage(m, eventData)));
+		}
+	}
+
+	/**
+	 * 将string 封装成method 想要的参数类型
+	 *
+	 * @param eventData 原始数据
+	 * @param method    方法
+	 * @return 目标参数
+	 */
+	protected Object[] extractMessage(Method method, ZkEventData eventData) {
+		Class<?>[] types = method.getParameterTypes();
+		if (types.length == 1 && types[0].isInstance(ZkEventData.class)) {
+			return new Object[]{eventData};
+		} else if (types.length == 1 && types[0].isInstance(String.class)) {
+			return new Object[]{eventData.getData()};
+		} else if ((types.length == 1 && !types[0].isInstance(String.class))) {
+			return new Object[]{JSON.parseObject(eventData.getData(), types[0])};
+		} else {
+			return null;
+		}
+	}
+
+}
---
+++ palest-ink-spring-boot-starter-parent/kepler-zk-spring-boot-starter/src/main/java/com/palest/ink/zk/utils/Try.java
@@ -0,0 +1,107 @@
+package com.palest.ink.zk.utils;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.Nullable;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * 将受检异常吃掉
+ *
+ * @author WeiHui
+ * @version v1.0.0
+ * @date 2019/4/8 9:42
+ * @since JDK 1.8
+ */
+@Slf4j
+@UtilityClass
+public class Try {
+
+	public static <T> Consumer<T> ofConsumer(UncheckedConsumer<T> action) {
+		Objects.requireNonNull(action);
+		return t -> {
+			try {
+				action.accept(t);
+			} catch (Exception ex) {
+				log.error("ex ====> {}", ex);
+			}
+		};
+	}
+
+	public static <T> Supplier<T> ofSupplier(UncheckedSupplier<T> supplier, T defaultR) {
+		Objects.requireNonNull(supplier);
+		return () -> {
+			try {
+				return supplier.get();
+			} catch (Exception ex) {
+				log.error("ex ====> {}", ex);
+				return defaultR;
+			}
+		};
+	}
+
+	public static <T, R> Function<T, R> ofFunction(UncheckedFunction<T, R> mapper) {
+		Objects.requireNonNull(mapper);
+		return t -> {
+			try {
+				return mapper.apply(t);
+			} catch (Exception ex) {
+				throw new RuntimeException(ex);
+			}
+		};
+	}
+
+	public static <T, R> Function<T, R> ofFunction(UncheckedFunction<T, R> mapper, R defaultR) {
+		Objects.requireNonNull(mapper);
+		return t -> {
+			try {
+				return mapper.apply(t);
+			} catch (Exception ex) {
+				log.error("ex ====> {}", ex);
+				return defaultR;
+			}
+		};
+	}
+
+	@FunctionalInterface
+	public interface UncheckedConsumer<T> {
+
+		/**
+		 * Performs this operation on the given argument.
+		 *
+		 * @param t the input argument
+		 */
+		void accept(T t) throws Exception;
+
+	}
+
+	@FunctionalInterface
+	public interface UncheckedFunction<T, R> {
+
+		/**
+		 * Applies this function to the given argument.
+		 *
+		 * @param t the function argument
+		 * @return the function result
+		 */
+		R apply(@Nullable T t) throws Exception;
+
+	}
+
+	@FunctionalInterface
+	public interface UncheckedSupplier<T> {
+
+		/**
+		 * Gets a result.
+		 *
+		 * @return a result
+		 */
+		T get() throws Exception;
+
+	}
+
+}
