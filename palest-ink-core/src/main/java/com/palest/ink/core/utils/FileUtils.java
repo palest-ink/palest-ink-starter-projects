ff70414fd0d9a524d546e3d2bd493780e612ec03
huwenxuan <dearjohn@aliyun.com>
2020-01-21 17:48:27 +0800

    fix name

---
+++ palest-ink-core/src/main/java/com/palest/ink/core/utils/FileUtils.java
@@ -0,0 +1,15 @@
+package com.palest.ink.core.utils;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * file-utilities
+ *
+ * @author huwenxuan
+ * @date 2020/1/21.
+ */
+@UtilityClass
+public class FileUtils extends org.apache.commons.io.FileUtils {
+
+
+}
---
+++ palest-ink-core/src/main/java/com/palest/ink/core/utils/StringUtils.java
@@ -0,0 +1,11 @@
+package com.palest.ink.core.utils;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * @author huwenxuan
+ * @date 2020/1/21.
+ */
+@UtilityClass
+public class StringUtils extends org.apache.commons.lang3.StringUtils {
+}
